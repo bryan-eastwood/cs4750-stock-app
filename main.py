6a95e678d35bdf91ab8a369eff81d9876e303fc2
Bryan Eastwood <bryan@bryans-mbp.home>
2021-11-23 23:59:06 -0500

    Additions

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
 from flask import render_template
+from datetime import *; from dateutil.relativedelta import *
+import time
 
 app = Flask(__name__)
 
@@ -18,3 +20,25 @@ cursor = mydb.cursor()
 def hello():
     #cursor.execute("SELECT * FROM trade")
     return render_template("index.html")
+
+@app.route('/chart')
+def chart():
+  return render_template("chart.html")
+
+@app.route('/data')
+def data():
+  latest = datetime.fromtimestamp(1628523000)
+  try: 
+    cursor.execute("SELECT * FROM price_data WHERE datetime > %s AND ticker = %s;", ((latest-relativedelta(days=int(request.args.get('days')))).strftime("%s"), request.args.get('ticker')))
+  except BaseException as e:
+    print(e)
+    print(cursor.statement)
+  data = []
+  for row in cursor:
+    data.append({
+      'ticker': row[0],
+      'time': row[1],
+      'price': row[2],
+      'vol': row[3]
+    })
+  return jsonify(data)
