cf89494e1676e18647bcda50a59661ec299c9e87
Your Name <rezasharp12@gmail.com>
2021-12-01 13:47:08 -0500

    booga booga

--- main.py
+++ main.py
@@ -26,23 +26,9 @@ def hello():
 
 @app.route('/chart', methods =  ['GET', 'POST'])
 def chart():
-<<<<<<< HEAD
-  
-  return render_template("chart.html")
-=======
   stock = request.args['ticker']
   return make_response(render_template('chart.html', ticker = stock))
 
-@app.route('/social')
-def social():
- username = request.cookies.get("userID")
- cursor.execute("SELECT * FROM trade NATURAL JOIN user WHERE %s ORDER BY datetime", (username,)) # get my trades.
- my_trades = []
- for x in cursor:
-   my_trades.append(x)
- print (my_trades)
- return render_template("social.html", mytrades=my_trades)
->>>>>>> f2b550fbc6d7a45935c1bb97bd89e32ca3f4b71a
 
 @app.route('/social')
 def social():
@@ -52,7 +38,7 @@ def social():
   for x in cursor:
     my_trades.append(x)
   print (my_trades)
-  return render_template("social.html", mytrades=my_trades)
+  return render_template("social.html", usernamelogin=username, mytrades=my_trades)
 
 
 @app.route('/data')
@@ -73,18 +59,6 @@ def data():
     })
   return jsonify(data)
 
-<<<<<<< HEAD
-@app.route('/dashboard', methods =  ['GET', 'POST'])
-def dashboard():
-  if request.method == 'POST': 
-    username = request.form['usernamelogin']
-    password =  request.form['passwordlogin']
-  
-  try:
-
-    
-    cursor.execute("SELECT * FROM user WHERE username = %s AND password = %s;", (username, password))
-=======
  
 @app.route('/dashboard', methods =  ['GET', 'POST'])
 def dashboard():
@@ -93,7 +67,6 @@ def dashboard():
    password =  request.form['passwordlogin']
   try:
    cursor.execute("SELECT * FROM user WHERE username = %s AND password = %s;", (username, password))
->>>>>>> f2b550fbc6d7a45935c1bb97bd89e32ca3f4b71a
   except BaseException as e:
    print(e)
    print(cursor.statement)
@@ -103,13 +76,6 @@ def dashboard():
      'username': row[0]
   })
   if len(data) > 0:
-<<<<<<< HEAD
-    resp = make_response(render_template('dashboard.html', usernamelogin = request.form['usernamelogin']))#, username = curUser))
-    resp.set_cookie('userID', request.form['usernamelogin'])
-   
-    return resp
-    
-=======
    resp = make_response(render_template('dashboard.html', usernamelogin = request.form['usernamelogin']))
    resp.set_cookie('userID', request.form['usernamelogin'])
    return resp
@@ -119,7 +85,6 @@ def dashboard():
   #    username = request.cookies.get("userID")
   #  except BaseException as e:
   #    return render_template('index.html')
->>>>>>> f2b550fbc6d7a45935c1bb97bd89e32ca3f4b71a
   else:
    return render_template('index.html')
 
